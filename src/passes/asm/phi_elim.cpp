baf1f1b378d244f50ed4bfac97ba939b31d96b1c
Juni May <juni_may@outlook.com>
2023-07-28 12:33:47 +0800

    wip(passes): asm phi elim problem

--- src/passes/asm/phi_elim.cpp
+++ src/passes/asm/phi_elim.cpp
@@ -149,55 +149,6 @@ void phi_elim_function(FunctionPtr function, Builder& builder) {
                 li_instr->insert_next(fmv_instr);
               }
 
-            } else if (operand->is_local_memory()) {
-              // Local memory in phi is parameter
-              auto offset = std::get<LocalMemory>(operand->kind).offset;
-              auto asm_fp_id = builder.fetch_register(backend::Register{
-                backend::GeneralRegister::S0});
-              if (check_itype_immediate(offset)) {
-                auto ld_instruction = builder.fetch_load_instruction(
-                  backend::instruction::Load::Op::LD, phi_rd->id, asm_fp_id,
-                  builder.fetch_immediate(offset)
-                );
-                if (replace_pred_jmp) {
-                  builder.append_instruction(ld_instruction);
-                } else {
-                  auto exit_instr = new_bb->tail_instruction->prev.lock();
-                  while (exit_instr->is_branch_or_jmp() &&
-                         exit_instr != new_bb->head_instruction) {
-                    exit_instr = exit_instr->prev.lock();
-                  }
-                  exit_instr->insert_next(ld_instruction);
-                }
-              } else {
-                auto asm_tmp_id = builder.fetch_register(backend::Register{
-                  backend::GeneralRegister::T3});
-                auto li_instruction = builder.fetch_li_instruction(
-                  asm_tmp_id, builder.fetch_immediate(offset)
-                );
-                auto add_instruction = builder.fetch_binary_instruction(
-                  backend::instruction::Binary::Op::ADD, asm_tmp_id, asm_fp_id,
-                  asm_tmp_id
-                );
-                auto ld_instruction = builder.fetch_load_instruction(
-                  backend::instruction::Load::Op::LD, phi_rd->id, asm_tmp_id,
-                  builder.fetch_immediate(0)
-                );
-                if (replace_pred_jmp) {
-                  builder.append_instruction(li_instruction);
-                  builder.append_instruction(add_instruction);
-                  builder.append_instruction(ld_instruction);
-                } else {
-                  auto exit_instr = new_bb->tail_instruction->prev.lock();
-                  while (exit_instr->is_branch_or_jmp() &&
-                         exit_instr != new_bb->head_instruction) {
-                    exit_instr = exit_instr->prev.lock();
-                  }
-                  exit_instr->insert_next(li_instruction);
-                  li_instruction->insert_next(add_instruction);
-                  add_instruction->insert_next(ld_instruction);
-                }
-              }
             } else {
               auto li_instr =
                 builder.fetch_li_instruction(phi.rd_id, operand_id);
@@ -210,6 +161,55 @@ void phi_elim_function(FunctionPtr function, Builder& builder) {
                   exit_instr = exit_instr->prev.lock();
                 }
                 exit_instr->insert_next(li_instr);
+              }
+            }
+          } else if (operand->is_local_memory()) {
+            // Local memory in phi is parameter
+            auto offset = std::get<LocalMemory>(operand->kind).offset;
+            auto asm_fp_id = builder.fetch_register(backend::Register{
+              backend::GeneralRegister::S0});
+            if (check_itype_immediate(offset)) {
+              auto ld_instruction = builder.fetch_load_instruction(
+                backend::instruction::Load::Op::LD, phi_rd->id, asm_fp_id,
+                builder.fetch_immediate(offset)
+              );
+              if (replace_pred_jmp) {
+                builder.append_instruction(ld_instruction);
+              } else {
+                auto exit_instr = new_bb->tail_instruction->prev.lock();
+                while (exit_instr->is_branch_or_jmp() &&
+                       exit_instr != new_bb->head_instruction) {
+                  exit_instr = exit_instr->prev.lock();
+                }
+                exit_instr->insert_next(ld_instruction);
+              }
+            } else {
+              auto asm_tmp_id = builder.fetch_register(backend::Register{
+                backend::GeneralRegister::T3});
+              auto li_instruction = builder.fetch_li_instruction(
+                asm_tmp_id, builder.fetch_immediate(offset)
+              );
+              auto add_instruction = builder.fetch_binary_instruction(
+                backend::instruction::Binary::Op::ADD, asm_tmp_id, asm_fp_id,
+                asm_tmp_id
+              );
+              auto ld_instruction = builder.fetch_load_instruction(
+                backend::instruction::Load::Op::LD, phi_rd->id, asm_tmp_id,
+                builder.fetch_immediate(0)
+              );
+              if (replace_pred_jmp) {
+                builder.append_instruction(li_instruction);
+                builder.append_instruction(add_instruction);
+                builder.append_instruction(ld_instruction);
+              } else {
+                auto exit_instr = new_bb->tail_instruction->prev.lock();
+                while (exit_instr->is_branch_or_jmp() &&
+                       exit_instr != new_bb->head_instruction) {
+                  exit_instr = exit_instr->prev.lock();
+                }
+                exit_instr->insert_next(li_instruction);
+                li_instruction->insert_next(add_instruction);
+                add_instruction->insert_next(ld_instruction);
               }
             }
           } else {
