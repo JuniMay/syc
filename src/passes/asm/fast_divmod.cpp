c8a1651e3f52772999ba49cee0f5d6c3c8a29093
SynodicMonth <Synodic_Month@163.com>
2023-08-14 16:25:36 +0800

    feat(fast_divmod): implement fastmod

--- src/passes/asm/fast_divmod.cpp
+++ src/passes/asm/fast_divmod.cpp
@@ -280,9 +280,280 @@ void fast_divmod_basic_block(BasicBlockPtr basic_block, Builder& builder) {
         curr_instr = next_instr;
         next_instr = next_instr->next;
         curr_instr->remove(builder.context);
+      } else if (curr_li->rd_id == next_binary->rs2_id && next_binary->op == Binary::REMW) {
+        // li v0, c
+        // divw v2, v1, v0
+        auto immediate = std::get<Immediate>(
+          builder.context.get_operand(curr_li->imm_id)->kind
+        );
+        if (immediate.get_value() > INT32_MAX || immediate.get_value() < INT32_MIN) {
+          throw std::runtime_error("fast_divmod: immediate value out of range");
+        }
+        int immediate_value = (int)immediate.get_value();
+        if (immediate_value == 0) {
+          throw std::runtime_error("fast_divmod: immediate value cannot be zero");
+        }
+        auto next_dst = builder.context.get_operand(next_binary->rd_id);
+        auto next_rs1 = builder.context.get_operand(next_binary->rs1_id);
+        auto next_rs2 = builder.context.get_operand(next_binary->rs2_id);
+        ms magic_number = magic(immediate_value);
+        std::cout << "magic number: " << magic_number.M << ", " << magic_number.s << std::endl;
+        if (immediate_value >= 2) {
+          if (magic_number.s > 0) {
+            // if magic_number.s > 0
+            // li vc, c
+            // rem v2, v1, vc
+            // ->
+            // li v8, magic_number.M
+            // mul v3, v1, v8
+            // srai v4, v3, 32
+            // addw v5, v4, v1
+            // sraiw v6, v5, magic_number.s 
+            // srliw v7, v1, 31
+            // addw v8, v7, v6
+            // li v10, c
+            // mulwi v9, v8, v10
+            // subw v2, v1, v9
+            // in reverse order
+            auto v3 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v4 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v5 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v6 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v7 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v8 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v9 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v10 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::SUBW, next_dst->id, next_rs1->id, v9)
+            );
+            next_instr->insert_next(  
+              builder.fetch_binary_instruction(Binary::MULW,  v9, v8, v10)
+            );
+            next_instr->insert_next(
+              builder.fetch_li_instruction(
+                v10, builder.fetch_immediate(immediate_value)
+              )
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::ADDW, v8, v7, v6)
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_imm_instruction(
+                BinaryImm::SRLIW, v7, next_rs1->id, builder.fetch_immediate(31))
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_imm_instruction(
+                BinaryImm::SRAIW, v6, magic_number.M < 0 ? v5 : v4, builder.fetch_immediate(magic_number.s))
+            );
+            if (magic_number.M < 0)
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::ADDW, v5, v4, next_rs1->id)
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_imm_instruction(
+                BinaryImm::SRAI, v4, v3, builder.fetch_immediate(32)
+              )
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::MUL, v3, next_rs1->id, v8)
+            );
+            next_instr->insert_next(
+              builder.fetch_li_instruction(
+                v8, builder.fetch_immediate(magic_number.M)
+              )
+            );
+          } else {
+            // if magic_number.s <= 0
+            // div v2, v1, c
+            // ->
+            // li v8, magic_number.M
+            // mul v3, v1, v8
+            // srai v4, v3, 32
+            // addw v5, v4, v1
+            // srliw v7, v1, 31
+            // addw v8, v7, v5
+            // li v10, c
+            // mulw v9, v8, v10
+            // subw v2, v1, v9
+            // in reverse order
+            auto v3 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v4 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v5 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v7 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v8 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v9 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v10 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::SUBW, next_dst->id, next_rs1->id, v9)
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::MULW, v9, v8, v10)
+            );
+            next_instr->insert_next(
+              builder.fetch_li_instruction(
+                v10, builder.fetch_immediate(immediate_value)
+              )
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::ADDW, v8, v7, magic_number.M < 0 ? v5 : v4)
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_imm_instruction(
+                BinaryImm::SRLIW, v7, next_rs1->id, builder.fetch_immediate(31))
+            );
+            if (magic_number.M < 0)
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::ADDW, v5, v4, next_rs1->id)
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_imm_instruction(
+                BinaryImm::SRAI, v4, v3, builder.fetch_immediate(32)
+              )
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::MUL, v3, next_rs1->id, v8)
+            );
+            next_instr->insert_next(
+              builder.fetch_li_instruction(
+                v8, builder.fetch_immediate(magic_number.M)
+              )
+            );
+          }
+        } else if (immediate_value <= -2) {
+          if (magic_number.s > 0) {
+            // if magic_number.s > 0
+            // div v2, v1, c
+            // ->
+            // li v8, magic_number.M
+            // mul v3, v1, v8
+            // srai v4, v3, 32
+            // subw v5, v4, v1
+            // sraiw v6, v5, magic_number.s 
+            // srliw v7, v1, 31
+            // addw v8, v7, v6
+            // li v10, c
+            // mulw v9, v8, v10
+            // subw v2, v1, v9
+            // in reverse order
+            auto v3 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v4 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v5 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v6 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v7 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v8 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v9 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v10 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::SUBW, next_dst->id, next_rs1->id, v9)
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::MULW, v9, v8, v10)
+            );
+            next_instr->insert_next(
+              builder.fetch_li_instruction(
+                v10, builder.fetch_immediate(immediate_value)
+              )
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::ADDW, v8, v7, v6)
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_imm_instruction(
+                BinaryImm::SRLIW, v7, next_rs1->id, builder.fetch_immediate(31))
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_imm_instruction(
+                BinaryImm::SRAIW, v6, magic_number.M < 0 ? v5 : v4, magic_number.s
+              )
+            );
+            if (magic_number.M < 0)
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::SUBW, v5, v4, next_rs1->id)
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_imm_instruction(
+                BinaryImm::SRAI, v4, v3, builder.fetch_immediate(32)
+              )
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::MUL, v3, next_rs1->id, v8)
+            );
+            next_instr->insert_next(
+              builder.fetch_li_instruction(
+                v8, builder.fetch_immediate(magic_number.M)
+              )
+            );
+          } else {
+            // if magic_number.s <= 0
+            // div v2, v1, c
+            // ->
+            // li v8, magic_number.M
+            // mul v3, v1, v8
+            // srai v4, v3, 32
+            // subw v5, v4, v1
+            // srliw v7, v1, 31
+            // addw v8, v7, v5
+            // li v10, c
+            // mulw v9, v8, v10
+            // subw v2, v1, v9
+            // in reverse order
+            auto v3 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v4 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v5 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v7 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v8 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v9 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+            auto v10 = builder.fetch_virtual_register(VirtualRegisterKind::General);
+
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::SUBW, next_dst->id, next_rs1->id, v9)
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::MULW, v9, v8, v10)
+            );
+            next_instr->insert_next(
+              builder.fetch_li_instruction(
+                v10, builder.fetch_immediate(immediate_value)
+              )
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::ADDW, v8, v7, magic_number.M < 0 ? v5 : v4)
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_imm_instruction(
+                BinaryImm::SRLIW, v7, next_rs1->id, builder.fetch_immediate(31))
+            );
+            if (magic_number.M < 0)
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::SUBW, v5, v4, next_rs1->id)
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_imm_instruction(
+                BinaryImm::SRAI, v4, v3, builder.fetch_immediate(32)
+              )
+            );
+            next_instr->insert_next(
+              builder.fetch_binary_instruction(Binary::MUL, v3, next_rs1->id, v8)
+            );
+            next_instr->insert_next(
+              builder.fetch_li_instruction(
+                v8, builder.fetch_immediate(magic_number.M)
+              )
+            );
+          }
+        } else {
+          throw std::runtime_error("fast div not implemented");
+        }
+
+        curr_instr->remove(builder.context);
+        curr_instr = next_instr;
+        next_instr = next_instr->next;
+        curr_instr->remove(builder.context);
       }
     }
-
     curr_instr = next_instr;
   }
 }
